2cd54ee29fa1d448626d9b04e202dc4edf0d2a16
Pavitra <asthanapavitra@gmail.com>
2024-09-17 18:45:16 +0530

    changed little code

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Enable CORS
+app.use(cors());
 app.use(express.json()); // Parse incoming JSON requests
 
-// Routes
+// Use the /api prefix for all routes
 app.use('/api', bookRoutes);
+
+// Default route to handle '/'
+app.get('/', (req, res) => {
+  res.send('Welcome to My Bookshelf API');
+});
 
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+export default app;
