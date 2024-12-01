20061f9ab0363bacc11df035646da6b1c2bc3ba0
asthanapavitra <124436931+asthanapavitra@users.noreply.github.com>
2024-12-01 11:00:31 +0530

    Update fetchBookCover.js

--- utils/fetchBookCover.js
+++ utils/fetchBookCover.js
@@ -7,6 +7,9 @@ async function fetchBookCover(title) {
 
     if (searchResults.length > 0) {
       const coverId = searchResults[0].cover_i;
+      if (!coverId) {
+        return 'https://covers.openlibrary.org/b/id/8225261-L.jpg'; // Default cover
+      }
       return `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`;
     } else {
       return 'https://covers.openlibrary.org/b/id/8225261-L.jpg';
